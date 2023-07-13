7d6f879f1d9fd3ea48aef8c7fb8402106449fb0b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-07-13 16:20:07 +0700

    remove margin modal

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -21,7 +21,7 @@
       >
         <div class="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
-            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <div class="mt-3 text-center sm:mt-0 sm:text-left">
               <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">
                 {{ title }}
               </h3>
