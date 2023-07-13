00f5f36ccf9e1db3af0bc04d0267398fd427d22d
7ELEVEN\kuankeehan <kuankeehan@gosoft.co.th>
2023-07-13 14:42:34 +0700

    restructure

--- src/views/product/ProductDetailsView.vue
+++ src/views/product/ProductDetailsView.vue
@@ -5,13 +5,18 @@
             <ProductItemLoader v-show="state.loader" />
         </div>
         <div class="col-span-12 md:col-span-8">
-            <div v-show="!state.loader">
-                <p class="text-gray-600 text-sm pr-4 mb-4">
+            <div v-show="!state.loader" class="flex flex-col gap-y-4">
+                <p class="text-gray-600 text-sm pr-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. A magni modi sequi aliquid. Quos iste voluptatem laudantium quia, at ad et facilis, odio, ab accusamus delectus quae repellendus vel a!
                 </p>
-                <ProductIncrement class="mb-4" />
-                <button class="btn-primary">
-                    <RouterLink to="/checkout">Add to cart</RouterLink>                
+                <div>
+                    <ProductIncrement :quantity="state.quantity" @change="productIncrementChange($event)" />
+                    <div class="flex flex-col" v-for="error in $v.quantity.$errors" v-bind:key="error.$uid">
+                        <span class="text-red-500">{{ error.$message }}</span>
+                    </div>
+                </div>
+                <button class="btn-primary w-[30px]" v-on:click="navigateToCartPage">
+                    Add to cart
                 </button>
             </div>
             <div v-show="state.loader">
@@ -28,32 +33,53 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink, useRoute } from 'vue-router';
-import ProductItem from "@/components/ProductItemComponent.vue";
-import ProductItemLoader from "@/components/ProductItemLoaderComponent.vue";
-import ProductIncrement from "@/components/ProductIncrementComponent.vue";
+import { useRoute, useRouter } from 'vue-router';
+import ProductItem from "@/components/product/ProductItemComponent.vue";
+import ProductItemLoader from "@/components/product/ProductItemLoaderComponent.vue";
+import ProductIncrement from "@/components/product/ProductIncrementComponent.vue";
 import { product } from '@/http';
 import type { ProductResponse } from '@/models';
 import { reactive } from 'vue';
 import { SimulationDelay } from "@/configs";
+import useVuelidate from '@vuelidate/core';
+import { required, minValue } from '@vuelidate/validators';
 
 type StateModel = {
     productDetails: ProductResponse | null,
+    quantity: number,
     loader: boolean
 }
 
+const router = useRouter()
 const route = useRoute()
+
 const productId = route.params.id.toString()
 const state = reactive<StateModel>({
     productDetails: null,
+    quantity: 0,
     loader: true
 })
+
+const $v = useVuelidate({
+    quantity: { required, minValue: minValue(1) }
+},state)
 
 const loadProductDetails = () => {
     state.loader = true
     product().details(productId)
         .then(response => state.productDetails = response)
         .finally(() => state.loader = false)
+}
+
+const productIncrementChange = (quantity: number) => {
+    state.quantity = quantity
+}
+
+const navigateToCartPage = async () => {
+    const valid = await $v.value.$validate()
+    if (!valid)
+        return
+    router.push({path: '/checkout'})
 }
 
 setTimeout(loadProductDetails, SimulationDelay);
